1da91a4f3e5577134fee9014f56033acce8e5550
Amirkhon3223 <amirich3223@outlook.com>
2023-05-26 18:18:06 +0500

    Relase version 1.1

--- src/app/services/order-details.service.ts
+++ src/app/services/order-details.service.ts
@@ -11,102 +11,77 @@ export class OrderDetailsService {
   furnitureDetails = [
     {
       id: 1,
-      furnituresName: "Стулчахо",
-      furnituresDetail: "Далер кашкар месозад, удобный зур Далер кашкар месозад, удобный зур Далер кашкар месозад, удобный зур Далер кашкар месозад, удобный зур Далер кашкар месозад, удобный зур Далер кашкар месозад, удобный зур",
-      furnituresPrice: 200,
-      furnituresInStock: "Available: InStock",
+      furnituresName: "Стол с стулчиками",
+      furnituresDetail: "Стол с стулчиками для кухни, минималистичный и комфортный дизайн. Яркий и красивый вид сочетния этих стлуьем с столом, прекрасно подходит для вашей яркой кухней. Место где стоит этот стол и стулья будет выделяться в вашей куханной комнате",
+      furnituresPrice: 680,
+      furnituresInStock: "Доступно: В наличии",
       furnituresImg: "../../../assets/images/Product.png"
     },
     {
       id: 2,
-      furnituresName: "Стулчахо",
-      furnituresDetail: "Далер кашкар месозад, удобный зур Далер кашкар месозад, удобный зур Далер кашкар месозад, " +
-        "удобный зур Далер кашкар месозад, удобный зур Далер кашкар месозад," +
-        "удобный зур Далер кашкар месозад, удобный зур" +
-        "LoremLorem ipsum dolor sit amet, consectetur adipisicing elit. At ipsa obcaecati placeat voluptates? Autem culpa distinctio expedita,Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ipsa obcaecati placeat voluptates? Autem culpa distinctio expedita, ipsum iste molestiae nobis officiis quod reiciendis sit! Accusantium eaque ipsam odio officia? ipsum iste molestiae nobis officiis quod reiciendis sit! Accusantium eaque ipsam odio officia?",
-      furnituresPrice: 200,
-      furnituresInStock: "Available: InStock",
+      furnituresName: "Стул",
+      furnituresDetail: "Стул для школьной парты. Идеальная форма и вид делает его не только удобным чтоб сидеть на неём, но и заботится о анатомии. Правильная форма позволяет дольше сидеть и не вредить организму",
+      furnituresPrice: 180,
+      furnituresInStock: "Доступно: В наличии",
       furnituresImg: "../../../assets/images/Product1.png"
     },
     {
       id: 3,
-      furnituresName: "Стулчахо",
-      furnituresDetail: "Далер кашкар месозад, удобный зур Далер кашкар месозад, удобный зур Далер кашкар месозад, удобный зур Далер кашкар месозад, удобный зур Далер кашкар месозад, удобный зур Далер кашкар месозад, удобный зур",
-      furnituresPrice: 200,
-      furnituresInStock: "Available: InStock",
+      furnituresName: "Маленький шкаф",
+      furnituresDetail: "Шкафчик не смотря на маленький и скромный вид, имеет хорошую вместительность вещей. Также над ним тоже можно что-нибудь поставить цветы или лампу. Что делает вид блее эстетичным и минималистичным",
+      furnituresPrice: 430,
+      furnituresInStock: "Доступно: В наличии",
       furnituresImg: "../../../assets/images/Product12.png"
     },
     {
       id: 4,
-      furnituresName: "Стулчахо",
-      furnituresDetail: "Далер кашкар месозад, удобный зур Далер кашкар месозад, удобный зур Далер кашкар месозад, удобный зур",
-      furnituresPrice: 200,
-      furnituresInStock: "Available: InStock",
+      furnituresName: "Гостевой стул",
+      furnituresDetail: "Гостевой стул, хорошо будет смотреться в гостинной. И на ней можно удобно сидеть и отдыхать. Играть в Play Station например.",
+      furnituresPrice: 270,
+      furnituresInStock: "Доступно: В наличии",
       furnituresImg: "../../../assets/images/Product3.png"
     },
     {
       id: 5,
-      furnituresName: "Стулчахо",
-      furnituresDetail: "Далер кашкар месозад, удобный зур",
-      furnituresPrice: 200,
-      furnituresInStock: "Available: InStock",
+      furnituresName: "Кухонная гарнитура",
+      furnituresDetail: "Кухонная гарнитура(или Мебель для кухни). Можете сами выбрать материал изготовление, по качеству и виду. Сделаем любые мебели для вашей кухни и по вашему дизайну",
+      furnituresPrice: "",
+      furnituresInStock: "Доступно: В наличии",
       furnituresImg: "../../../assets/images/Product4.png"
     },
     {
       id: 6,
-      furnituresName: "Стулчахо",
-      furnituresDetail: "Далер кашкар месозад, удобный зур",
-      furnituresPrice: 200,
-      furnituresInStock: "Available: InStock",
+      furnituresName: "Стул",
+      furnituresDetail: "Стул для гостинной или для офисов, также хорошо будет смотреться в переговорных кабинетах." +
+        "Удобная и практичная",
+      furnituresPrice: 270,
+      furnituresInStock: "Доступно: В наличии",
       furnituresImg: "../../../assets/images/Product5.png"
     },
     {
       id: 7,
-      furnituresName: "Стулчахо",
-      furnituresDetail: "Далер кашкар месозад, удобный зур",
+      furnituresName: "Большая тумбочка",
+      furnituresDetail: "Большая тумбочка под большим телевизором. Создает уют и хорошую атмосферу для вашей комнаты",
       furnituresPrice: 200,
-      furnituresInStock: "Available: InStock",
+      furnituresInStock: "Доступно: В наличии",
       furnituresImg: "../../../assets/images/Product6.png"
     },
     {
       id: 8,
-      furnituresName: "Стулчахо",
-      furnituresDetail: "Далер кашкар месозад, удобный зур",
-      furnituresPrice: 200,
-      furnituresInStock: "Available: InStock",
+      furnituresName: "Барные стулья",
+      furnituresDetail: "Барные стулья для вашей кухни, особо широко распротренно в странах европы, у которых столешнцы высокие ",
+      furnituresPrice: 180,
+      furnituresInStock: "Доступно: В наличии",
       furnituresImg: "../../../assets/images/Product7.png"
     },
     {
       id: 9,
-      furnituresName: "Стулчахо",
-      furnituresDetail: "Далер кашкар месозад, удобный зур",
-      furnituresPrice: 200,
-      furnituresInStock: "Available: InStock",
+      furnituresName: "Стул для гостинной",
+      furnituresDetail: "Стул для гостинной или для офисов, также хорошо будет смотреться в переговорных кабинетах." +
+        "Удобная и практичная",
+      furnituresPrice: 270,
+      furnituresInStock: "Доступно: В наличии",
       furnituresImg: "../../../assets/images/Product8.png"
-    },
-    {
-      id: 10,
-      furnituresName: "Стулчахо",
-      furnituresDetail: "Далер кашкар месозад, удобный зур",
-      furnituresPrice: 200,
-      furnituresInStock: "Available: InStock",
-      furnituresImg: "../../../assets/images/Product9.png"
-    },
-    {
-      id: 11,
-      furnituresName: "Стулчахо",
-      furnituresDetail: "Далер кашкар месозад, удобный зур",
-      furnituresPrice: 200,
-      furnituresInStock: "Available: InStock",
-      furnituresImg: "../../../assets/images/Product10.png"
-    },
-    {
-      id: 12,
-      furnituresName: "Стулчахо",
-      furnituresDetail: "Далер кашкар месозад, удобный зур",
-      furnituresPrice: 200,
-      furnituresInStock: "Available: InStock",
-      furnituresImg: "../../../assets/images/Product11.png"
     },
   ]
 
